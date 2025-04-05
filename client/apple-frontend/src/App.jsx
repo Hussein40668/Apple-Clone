43108fcac179f8182a54d709d9f6df4decac05d4
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-04-05 17:04:59 +0300

    configure the App.jsx file

--- client/apple-frontend/src/App.jsx
+++ client/apple-frontend/src/App.jsx
@@ -1,13 +1,42 @@
-import './App.css'
+// bootstrap
+import "./commonResource/css/bootstrap.css";
+import "./commonResource/js/bootstrap.js";
+import "./components/Footer/custom.js";
+
+import Main from "./Pages/Main/Main.jsx";
+import Mac from "./Pages/Mac/Mac.jsx";
+import Iphone from "./Pages/Iphone/Iphone.jsx";
+import Ipad from "./Pages/Ipad/Ipad.jsx";
+import Watch from "./Pages/Watch/Watch.jsx";
+import TV from "./Pages/TV/TV.jsx";
+import Music from "./Pages/Music/Music.jsx";
+import Support from "./Pages/Support/Support.jsx";
+import Cart from "./Pages/Cart/Cart.jsx";
+import Search from "./Pages/Search/Search.jsx";
+import SharedLayout from "./components/SharedLayout/SharedLayout.jsx";
+import SingleAppleProduct from "./Pages/SingleAppleProduct/SingleAppleProduct.jsx"
+import Four04 from "./Pages/Four04/Four04.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
-    <>
-  hi
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/mac" element={<Mac />} />
+        <Route path="/iphone" element={<Iphone />} />
+        <Route path="/iphone/:productID" element={<SingleAppleProduct />} />
+        <Route path="/ipad" element={<Ipad />} />
+        <Route path="/watch" element={<Watch />} />
+        <Route path="/tv" element={<TV />} />
+        <Route path="/music" element={<Music />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Four04 />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
